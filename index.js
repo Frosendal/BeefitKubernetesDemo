3e536a11a49c478c8e6ec0c0a52e162d63788b0a
roman800 <magnus.roman@hotmail.com>
2021-08-12 17:26:23 +0200

    Added yaml & kubernetes & updated api with function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const express = require('express');
+var os = require("os");
+var hostname = os.hostname();
 
 const app = express();
 
@@ -10,10 +12,21 @@ router.get('/', function (req, res) {
     res.status(200).json('Ass')
     
 })
+
+router.get('/kill', function(req, res){
+    console.error("succesfully dead: " + hostname)
+    res.status(200).json("succesfully dead: "+ hostname)
+    process.exit();
+})
+
+router.get('/name', function(req, res){
+    console.log("I am: " + hostname)
+    res.status(200).json("I am: "+ hostname)
+})
+
+
 app.use('/api', router);
 
 app.listen(5000, () => {
     console.log('Server listening on port 5000.')
 })
-
-// Kill route to demonstrate? 
